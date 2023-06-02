21218b7ce7935c252e621f285ac78fae622b3371
abrarabedin001 <abrarabedin001@gmail.com>
2023-06-02 10:22:39 -0700

    test2

--- next.config.mjs
+++ next.config.mjs
@@ -17,13 +17,7 @@ const config = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+  }
+  
 };
 export default config;
